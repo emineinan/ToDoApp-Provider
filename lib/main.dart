633f8ebfef93e59f7d74562f92b8d826c808b9d4
Emine İnan <emineinanx@gmail.com>
2020-12-01 17:21:23 +0300

    box decoration added.

--- lib/main.dart
+++ lib/main.dart
@@ -23,20 +23,61 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[900],
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text(
-          "Todo App",
-          style: TextStyle(color: Colors.white),
-        ),
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white70,
+      appBar: buildAppBar(),
+      body: Column(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "02 : 36 PM",
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "current time",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
           ),
-          onPressed: () {},
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    topLeft: Radius.circular(50)),
+                color: Colors.white),
+          ))
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Text(
+        "Todo App",
+        style: TextStyle(color: Colors.white),
+      ),
+      leading: IconButton(
+        icon: Icon(
+          Icons.menu,
+          color: Colors.white70,
         ),
+        onPressed: () {},
       ),
     );
   }
