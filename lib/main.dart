6091df8121d912451014d46d48728fca394fee54
Emine İnan <emineinanx@gmail.com>
2020-12-01 17:11:37 +0300

    appbar added.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ToDo APP',
       debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[900],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Todo App",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white70,
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
